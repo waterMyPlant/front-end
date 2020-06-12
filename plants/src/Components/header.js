413baf79a4b98fcf00d291b2713ad82f3b354f78
Brianna Workman <brianna.workman@hotmail.com>
2020-06-12 17:36:46 -0400

    centers plant page and fixes logout nav button

--- plants/src/Components/header.js
+++ plants/src/Components/header.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
-import logOut from '../Components/LogOut';
+import LogOut from './logOut';
 
 const Header = () => {
-	return (
-		<div className='header'>
-			<nav className='nav_bar'>
-				<Link className='titleNav' to='/'>
-					<h1>Home</h1>
-				</Link>
-				<Link className='titleNav' to='/facts'>
-					<h1>List of Facts</h1>
-				</Link>
-				<Link className='titleNav' to='/plants'>
-					<h1>List of Plants</h1>
-				</Link>
-				<logOut />
-			</nav>
-		</div>
-	);
+  return (
+    <div className='header'>
+      <nav className='nav_bar'>
+        <Link className='titleNav' to='/'>
+          <h1>Home</h1>
+        </Link>
+        <Link className='titleNav' to='/facts'>
+          <h1>List of Facts</h1>
+        </Link>
+        <Link className='titleNav' to='/plants'>
+          <h1>List of Plants</h1>
+        </Link>
+        <LogOut />
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
--- plants/src/Components/logOut.js
+++ plants/src/Components/logOut.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const logOut = () => {
-	return (
-		<Link className='titleNav' to='/' onClick={() => localStorage.clear()}>
-			Logout
-		</Link>
-	);
+  return (
+    <Link className='titleNav' to='/' onClick={() => localStorage.clear()}>
+      <h1>Log Out</h1>
+    </Link>
+  );
 };
 
 export default logOut;
