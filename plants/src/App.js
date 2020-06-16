2ccb927756a5fcb0ccd9665863a451719d3ac12e
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-06-15 20:00:24 -0400

    changed plant facts background picture

--- plants/src/App.js
+++ plants/src/App.js
@@ -6,18 +6,18 @@ import Header from './Components/header';
 import plantFacts from './Components/plantFacts';
 import { Route } from 'react-router-dom';
 import PrivateRoute from './utils/PrivateRoute';
-import PlantInfo from './Components/plantInfo';
+import plantInfo from './Components/plantInfo';
 
 function App(props) {
-  return (
-    <div className='container'>
-      <Header />
-      <Route exact path='/signIn' component={SignIn} />
-      <Route exact path='/' component={SignUp} />
-      <PrivateRoute exact path='/facts' component={plantFacts} />
-      <Route exact path='/plants' component={PlantInfo} />
-    </div>
-  );
+	return (
+		<div className='container'>
+			<Header />
+			<Route exact path='/signIn' component={SignIn} />
+			<Route exact path='/' component={SignUp} />
+			<PrivateRoute exact path='/facts' component={plantFacts} />
+			<Route exact path='/plants' component={plantInfo} />
+		</div>
+	);
 }
 
 export default App;
