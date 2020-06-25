4145cba674d318e5434224dd948c5097cad0ae26
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-06-25 18:14:47 -0400

    changes with josh

--- plants/src/Components/signIn.js
+++ plants/src/Components/signIn.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
@@ -16,122 +16,132 @@ import { loginUser } from '../actions';
 import { connect } from 'react-redux';
 
 const useStyles = makeStyles((theme) => ({
-  paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: '100%', // Fix IE 11 issue.
-    marginTop: theme.spacing(1),
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
+	paper  : {
+		marginTop     : theme.spacing(8),
+		display       : 'flex',
+		flexDirection : 'column',
+		alignItems    : 'center',
+	},
+	avatar : {
+		margin          : theme.spacing(1),
+		backgroundColor : theme.palette.secondary.main,
+	},
+	form   : {
+		width     : '100%', // Fix IE 11 issue.
+		marginTop : theme.spacing(1),
+	},
+	submit : {
+		margin : theme.spacing(3, 0, 2),
+	},
 }));
 
 function SignIn(props, tools) {
-  const classes = useStyles();
-  let history = useHistory();
+	const classes = useStyles();
+	let history = useHistory();
 
-  const [user, setUser] = useState({
-    username: '',
-    password: '',
-  });
+	const [ user, setUser ] = useState({
+		username : '',
+		password : '',
+	});
+	const [ isLoggingIn, setIsLoggingIn ] = useState(true);
+	useEffect(
+		() => {
+			setUser({
+				username : '',
+				password : '',
+			});
+		},
+		[ isLoggingIn ],
+	);
 
-  const handleChange = (event) => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+	useEffect(
+		() => {
+			console.log(user);
+		},
+		[ user ],
+	);
 
-  const handleSubmit = (event) => {
-    console.log('SIGN IN:', props);
-    event.preventDefault();
-    props.loginUser(user);
-    setUser({
-      username: '',
-      password: '',
-    });
-    history.push('/plants');
-  };
+	const handleChange = (event) => {
+		setUser({ ...user, [event.target.name]: event.target.value });
+	};
+	console.log('props login', props.login);
+	const handleSubmit = (event) => {
+		console.log('SIGN IN:', props);
+		event.preventDefault();
+		props.loginUser(user, setIsLoggingIn);
 
-  return (
-    <div className='signCont'>
-      <Container className='sign' component='main' maxWidth='xs'>
-        <h1>WATER MY PLANTS</h1>
-        <CssBaseline />
-        <div className={classes.paper}>
-          <Typography component='h1' variant='h5'>
-            Sign In
-          </Typography>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              id='username'
-              label='Username'
-              name='username'
-              autoComplete='username'
-              autoFocus
-              onChange={handleChange}
-              value={user.username}
-            />
-            <TextField
-              variant='outlined'
-              margin='normal'
-              required
-              fullWidth
-              name='password'
-              label='Password'
-              type='password'
-              id='password'
-              autoComplete='current-password'
-              onChange={handleChange}
-              value={user.password}
-            />
-            <FormControlLabel
-              control={<Checkbox value='remember' color='primary' />}
-              label='Remember me'
-            />
-            <Button
-              type='submit'
-              fullWidth
-              variant='contained'
-              color='primary'
-              className={classes.submit}
-              onClick={handleSubmit}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link href='#' variant='body2'>
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href='/' variant='body2'>
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
-          </form>
-        </div>
-      </Container>
-    </div>
-  );
+		history.push('/plants');
+	};
+
+	return (
+		<div className='signCont'>
+			<Container className='sign' component='main' maxWidth='xs'>
+				<h1>WATER MY PLANTS</h1>
+				<CssBaseline />
+				<div className={classes.paper}>
+					<Typography component='h1' variant='h5'>
+						Sign In
+					</Typography>
+					<form className={classes.form} noValidate>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							id='username'
+							label='Username'
+							name='username'
+							autoComplete='username'
+							autoFocus
+							onChange={handleChange}
+							value={user.username}
+						/>
+						<TextField
+							variant='outlined'
+							margin='normal'
+							required
+							fullWidth
+							name='password'
+							label='Password'
+							type='password'
+							id='password'
+							autoComplete='current-password'
+							onChange={handleChange}
+							value={user.password}
+						/>
+						<FormControlLabel control={<Checkbox value='remember' color='primary' />} label='Remember me' />
+						<Button
+							type='submit'
+							fullWidth
+							variant='contained'
+							color='primary'
+							className={classes.submit}
+							onClick={handleSubmit}>
+							Sign In
+						</Button>
+						<Grid container>
+							<Grid item xs>
+								<Link href='#' variant='body2'>
+									Forgot password?
+								</Link>
+							</Grid>
+							<Grid item>
+								<Link href='/' variant='body2'>
+									{"Don't have an account? Sign Up"}
+								</Link>
+							</Grid>
+						</Grid>
+					</form>
+				</div>
+			</Container>
+		</div>
+	);
 }
 
 const mapStateToProps = (state) => {
-  return {
-    login: state,
-  };
+	return {
+		login : state,
+	};
 };
 
 export default connect(mapStateToProps, { loginUser })(SignIn);
