b1cd9439dc07619c8e3e7cbddfff8d5eefa6c9e8
Jasmine Kasheboon Khoury <jasminekh96@gmail.com>
2020-06-30 13:06:54 -0400

    added plant facts but have to style

--- plants/src/Components/plantFacts.js
+++ plants/src/Components/plantFacts.js
@@ -7,6 +7,20 @@ function plantFacts() {
 		<div className='plantFactsCont'>
 			<Header />
 			<h1>Plant Facts</h1>
+			<p>
+				<span>OVERWATERING KILLS YOUR PLANTS</span>Stick your finger about an inch deep into any plant, when you take
+				your finger out if a lot of dirt sticks to your finger DO NOT WATER YOUR PLANT... BUT if you stick your finger
+				in and there's not much dirt sticking to your finger and it feels dry then it's time to water your plant.
+			</p>
+			<p>
+				Do not put new plants with your existing plants for 14 days. The nursery that you bought it from can have pests
+				and if you surround that new plant with your older plants you can end up giving your healthy plants pests.
+			</p>
+			<p>
+				Spring is the best time to repot your plants because with more sunlight they are going to grow faster, but only
+				go up a pot size 1-2 inches bigger, the bigger the pot the more possibility of root rot.
+			</p>
+			<p>If your plants roots are coiling around themselves that means it's time to repot!</p>
 		</div>
 	);
 }
